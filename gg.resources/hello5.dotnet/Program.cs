2536214ca96b2375aeb1eeab60f7c5f8169d2374
Milosz Krajewski <miloszkrajewski@o2.pl>
2022-08-16 00:15:09 +0100

    added simpleexec to .net example

--- gg.resources/hello5.dotnet/Program.cs
+++ gg.resources/hello5.dotnet/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using SimpleExec;
+
+Console.WriteLine(JsonSerializer.Serialize(new { Noun = "Hello", Verb = "World", Value = 1337 }));
+Console.WriteLine(JsonSerializer.Serialize(args));
+
+Command.Run("cmd", "/c dir");
